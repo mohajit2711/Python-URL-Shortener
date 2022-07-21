c9b78c7b696e2b34d6d82631bf7a0238934756c6
Mohajit Paul <80643937+mohajit2711@users.noreply.github.com>
2022-07-21 23:00:52 +0530

    URL-Shortener

---
+++ URL-Shortener.py
@@ -0,0 +1,16 @@
+import pyshorteners
+
+print("Hello, Welcome to Mohajit's URL shortener terminal! ")
+
+print("\n")
+
+long_link = input("Please enter the link you would like to shorten: ") #Asking for Long URL input
+
+pyshort = pyshorteners.Shortener() #using pyshorteners package
+
+short_link = pyshort.tinyurl.short(long_link) #shortening URL
+
+print("The shortened link is: " + short_link) #Shortened link displayed.
+
+
+# This code is contribute by Mohajit Paul
